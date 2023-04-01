b1766da2093971fe5b967f33f8d638088386d911
ArtX-code <poop402r@gmail.com>
2023-04-01 13:26:06 +0300

    нові функуії запису

--- app/database/db_executions.py
+++ app/database/db_executions.py
@@ -38,14 +38,97 @@ async def add_phone_number(id, value):
     await db.disconnect()
 
 
-async def email(id, value):
+async def add_email(id, value):
     await db.connect()
     await db.execute(f'''UPDATE public.resume_db1 SET email = '{value}' WHERE id = {id};''')
     await db.disconnect()
 
 
-async def education(id, value):
+async def add_education(id, value):
     await db.connect()
     await db.execute(f'''UPDATE public.resume_db1 SET education = '{value}' WHERE id = {id};''')
     await db.disconnect()
 
+
+async def add_lang(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET lang = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_lang_level(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET lang_level = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_country(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET country = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_city(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET city = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_description(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET description = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_profession(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET profession = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_soft_skills(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET soft_skills = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_tech_skills(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET tech_skills = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_projects(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET projects = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_how_long(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET how_long = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_job_description(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET job_description = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_past_work(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET past_work = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_password(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET password = '{value}' WHERE id = {id};''')
+    await db.disconnect()
+
+
+async def add_password(id, value):
+    await db.connect()
+    await db.execute(f'''UPDATE public.resume_db1 SET password = '{value}' WHERE id = {id};''')
+    await db.disconnect()
