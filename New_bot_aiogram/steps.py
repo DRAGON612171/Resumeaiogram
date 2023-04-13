0d73654a2cd5c37b1e0149a784fe7cdbf8cbc621
nazar <zvnazar2006@gmail.com>
2023-04-13 18:24:57 +0300

    1

--- New_bot_aiogram/steps.py
+++ New_bot_aiogram/steps.py
@@ -3,7 +3,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class Steps(StatesGroup):
     name_surname = State()
-    # updating_state = State()
     phone_number = State()
     get_email = State()
     get_education = State()
@@ -20,6 +19,23 @@ class Steps(StatesGroup):
     get_job_description = State()
     get_how_long = State()
     end_message = State()
+    # name_surname_edit = State()
+    # phone_number_edit = State()
+    # get_email_edit = State()
+    # get_education_edit = State()
+    # get_tech_skills_edit = State()
+    # get_soft_skills_edit = State()
+    # get_projects_edit = State()
+    # get_lang_edit = State()
+    # get_lang_level_edit = State()
+    # get_country_edit = State()
+    # get_city_edit = State()
+    # get_profession_edit = State()
+    # get_description_edit = State()
+    # get_work_experience_edit = State()
+    # get_job_description_edit = State()
+    # get_how_long_edit = State()
+    # end_message_edit = State()
 
 
 
